bbe5648fb22b5cc46ea1eca5d5d08763ce9d30c3
codergon <alphagon4@gmail.com>
2021-12-19 00:40:47 +0100

    Added ScrollText🦉

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import ElectionList from "./ElectionList";
+import Landing from "./Landing";
+
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <ElectionList />
+    </>
+  );
+};
+
+export default Home;
